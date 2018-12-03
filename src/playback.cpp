e8d8cee1fb54ff82ebd5a52dfcf24ccd21a592d2
Lionel Ott <lionel.ott@sydney.edu.au>
2018-12-04 10:51:34 +1100

    Playback program added

--- src/playback.cpp
+++ src/playback.cpp
@@ -1,6 +1,11 @@
 #include <cmath>
 #include <iostream>
+#include <fstream>
 #include <random>
+
+#include <boost/algorithm/string.hpp>
+#include <boost/function.hpp>
+#include <boost/lexical_cast.hpp>
 
 #include <franka/exception.h>
 #include <franka/robot.h>
@@ -9,7 +14,52 @@
 #include "quintic_interpolator.h"
 
 
-using lula::math;
+/**
+ * \brief Reads the content of a file with numerical data.
+ *
+ * The file is assumed to have the following format:
+ * v11 v12 ... v1n
+ * ...
+ * vn1 vn2 ... vnn
+ *
+ * \param fname file to read the data from
+ * \param split_pattern set of characters to split the input on
+ * \return data read from the file line by line
+ */
+template<typename T>
+std::vector<Eigen::Matrix<T, Eigen::Dynamic, 1>> read(
+        std::string const&              fname,
+        std::string const&              split_pattern=" "
+)
+{
+    typedef Eigen::Matrix<T, Eigen::Dynamic, 1> Vector_t;
+
+    std::vector<Vector_t> data;
+    std::ifstream input(fname);
+    std::string line;
+
+    while(input.good())
+    {
+        getline(input, line);
+
+        boost::trim(line);
+        std::vector<std::string> parts;
+        boost::split(parts, line, boost::is_any_of(split_pattern), boost::token_compress_on);
+
+        if(parts.size() > 1)
+        {
+            Vector_t values(parts.size());
+            for(size_t i=0; i<parts.size(); ++i)
+            {
+                values[i] = boost::lexical_cast<T>(parts[i]);
+            }
+            data.push_back(values);
+        }
+    }
+
+    return data;
+}
+
 
 
 class PlaybackMotionGenerator
@@ -18,6 +68,45 @@ class PlaybackMotionGenerator
         PlaybackMotionGenerator(std::string const& fname)
         {
             // Load data to play back on the robot
+            auto data = read<double>(fname, ",");
+
+            for(auto const& row : data)
+            {
+                Eigen::VectorXd q(7);
+                Eigen::VectorXd dq(7);
+                Eigen::VectorXd ddq(7);
+
+                if(row.size() != 21)
+                {
+                    std::cout << "Bad size of data" << std::endl;
+                }
+
+                q[0] = row[0];
+                q[1] = row[1];
+                q[2] = row[2];
+                q[3] = row[3];
+                q[4] = row[4];
+                q[5] = row[5];
+                q[6] = row[6];
+
+                dq[7] = row[7];
+                dq[8] = row[8];
+                dq[9] = row[9];
+                dq[10] = row[10];
+                dq[11] = row[11];
+                dq[12] = row[12];
+                dq[13] = row[13];
+
+                ddq[14] = row[14];
+                ddq[15] = row[15];
+                ddq[16] = row[16];
+                ddq[17] = row[17];
+                ddq[18] = row[18];
+                ddq[19] = row[19];
+                ddq[20] = row[20];
+
+                m_commands.push_back(lula::math::PosVelAcc(q, dq, ddq));
+            }
         };
 
         franka::JointPositions operator()(
@@ -27,42 +116,52 @@ class PlaybackMotionGenerator
         {
             m_time += period.toSec();
 
-            if(m_step_count++ % m_waypoint_distance == 0)
-            {
-                // Feed point to the interpolator
-                auto p = PosVelAcc(
-                    m_joint_positions[m_step_count],
-                    m_joint_velocities[m_step_count],
-                    m_joint_accelerations[m_step_count]
-                );
-                if(!m_interpolator.AddPt(m_time, p, m_last_error))
-                {
-                    std::cout << m_last_error << std::endl;
-                }
-            }
+            //if(m_step_count++ % m_waypoint_distance == 0)
+            //{
+            //    // Feed point to the interpolator
+            //    auto p = PosVelAcc(
+            //        m_joint_positions[m_step_count],
+            //        m_joint_velocities[m_step_count],
+            //        m_joint_accelerations[m_step_count]
+            //    );
+            //    if(!m_interpolator.AddPt(m_time, p, m_last_error))
+            //    {
+            //        std::cout << m_last_error << std::endl;
+            //    }
+            //}
         
-            auto p = PosVelAcc();
-            if(!interp.Eval(m_time, p, m_last_error))
-            {
-                std::cout << m_last_error << std::endl;
-            }
+            //if(!interp.Eval(m_time, p, m_last_error))
+            //{
+            //    std::cout << m_last_error << std::endl;
+            //}
 
             std::array<double, 7> q_command;
-            Eigen::VectorXd::Map(&q_command[0], 7) = p.x;
-            auto output = franka::JointPositions output(q_command);
-            output.motion_finished = motion_finished;
+            Eigen::VectorXd::Map(&q_command[0], 7) = m_commands[m_step_count].x;
+            //auto output = franka::JointPositions output(q_command);
+
+            franka::JointPositions output(q_command);
+            output.motion_finished = m_step_count >= m_commands.size();
+
+            m_step_count++;
+
             return output;
         };
+
+        std::array<double, 7> get_initial_pos() const
+        {
+            std::array<double, 7> q_command;
+            Eigen::VectorXd::Map(&q_command[0], 7) = m_commands[0].x;
+
+            return q_command;
+        }
 
     private:
         std::string                     m_last_error;
         int                             m_step_count = 0;
         int                             m_waypoint_distance = 10;
         double                          m_time = 0;
-        SequentialQuinticInterpolator   m_interpolator;
-        std::vector<Eigen::VectorXd>    m_joint_positions;
-        std::vector<Eigen::VectorXd>    m_joint_velocities;
-        std::vector<Eigen::VectorXd>    m_joint_accelerations;
+        //lula::math::SequentialQuinticInterpolator   m_interpolator;
+        std::vector<lula::math::PosVelAcc> m_commands;
 };
 
 
@@ -76,20 +175,29 @@ int main(int argc, char** argv)
     }
     try
     {
+        // Playback motion generator
+        auto playback_motion_generator = PlaybackMotionGenerator(argv[1]);
+
+        // Initialize robot connection
         franka::Robot robot(argv[1]);
         setDefaultBehavior(robot);
 
-        // First move the robot to a suitable joint configuration
-        std::array<double, 7> q_goal = {{0, -M_PI_4, 0, -3 * M_PI_4, 0, M_PI_2, M_PI_4}};
-        MotionGenerator motion_generator(0.5, q_goal);
-        std::cout << "WARNING: This example will move the robot! "
-                  << "Please make sure to have the user stop button at hand!" << std::endl
-                  << "Press Enter to continue..." << std::endl;
+        // Move robot to starting position of the playback sequence
+        auto motion_generator = MotionGenerator(
+                0.5,
+                playback_motion_generator.get_initial_pos()
+        );
+
+        std::cout << "WARNING: Robot will move to starting position "
+                     "make sure the estop is at hand." << std::endl;
+        std::cout << "Press ENTER to continue" << std::endl;
         std::cin.ignore();
 
-        // Execute the motion to start positoin
         robot.control(motion_generator);
+
         std::cout << "Finished moving to initial joint configuration." << std::endl;
+        std::cout << "Starting to execute trajectory" << std::endl;
+
 
         // Set additional parameters always before the control loop, NEVER in the control loop!
         // Set collision behavior.
@@ -104,74 +212,9 @@ int main(int argc, char** argv)
                 {{20.0, 20.0, 20.0, 25.0, 25.0, 25.0}}
         );
 
-        // Create random noise distribution
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_real_distribution<> jitter(-0.00001, 0.00001);
+        robot.control(playback_motion_generator);
 
-
-        // Initialize the initial position to the commanded one, to be close
-        // just in case
-        lula::math::SequentialQuinticInterpolator interp(
-            7,
-            (1.0 / 1000.0) * every_n_steps,
-            5.0,
-            500.0
-        );
-
-        std::array<double, 7> initial_position = q_goal;
-        double time = 0.0;
-        int steps = 0;
-        robot.control(
-            [&initial_position, &time, &gen, &jitter, &steps, &interp, amplitude, speed, every_n_steps]
-            (franka::RobotState const& robot_state, franka::Duration period) -> franka::JointPositions
-            {
-                // Set robot position to current state upon startup
-                if(time == 0.0)
-                {
-                    initial_position = robot_state.q_d;
-                }
-                time += period.toSec();
-
-                if(steps++ % every_n_steps == 0)
-                {
-                    auto offset = std::sin(time * speed) * amplitude;
-
-                    auto p = lula::math::PosVelAcc();
-                    //p.x = ;
-                    //p.xd = ;
-                    //p.xdd = ;
-                    
-                    std::string error;
-                    if(!interp.AddPt(time, p, error))
-                    {
-                        std::cout << error << std::endl;
-                    }
-                }
-
-                auto offset = std::sin(time * speed) * amplitude;
-                //auto offset = std::sin(time) * 0.7 + jitter(gen);
-                
-                // New position with jitter on one joint
-                franka::JointPositions output = {{
-                    initial_position[0],
-                    initial_position[1],
-                    initial_position[2],
-                    initial_position[3] + offset,
-                    initial_position[4],
-                    initial_position[5],
-                    initial_position[6]
-                }};
-
-                if(time >= 3.0)
-                {
-                    std::cout << std::endl << "Finished motion, shutting down noise example" << std::endl;
-                    return franka::MotionFinished(output);
-                }
-
-                return output;
-            }
-        );
+        std::cout << "Following trajectory completed" << std::endl;
     }
     catch (franka::Exception const& e)
     {
